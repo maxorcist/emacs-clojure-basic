6d40354c9d73e0d9e1296e7242b2e468a0381dd1
Max <maxsoderquist@gmail.com>
2018-01-30 13:22:53 +0100

    first commit

---
+++ init.el
@@ -0,0 +1,62 @@
+; init.el
+
+;; Author maxorcist
+;;
+;; Notes:
+;; This is meant solely for clojure development.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+(defvar my-packages 
+  '(
+    clojure-mode
+    clojure-mode-extra-font-locking
+    cider
+    counsel
+    ivy
+    flx
+    lispy
+    rainbow-delimiters
+    magit
+
+    ) "a list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;;;;
+;; Customization
+;;;;
+(add-to-list 'load-path "~/.emacs.d/customization")
+
+;; Sets up exec-path-from-shell so that Emacs will use the correct
+;; environment variables
+(load "shell-integration.el")
+
+;; These customizations make it easier for you to navigate files,
+;; switch buffers, and choose options from the minibuffer.
+(load "navigation.el")
+
+;; These customizations change the way emacs looks and disable/enable
+;; some user interface elements
+(load "ui.el")
+
+;; These customizations make editing a bit nicer.
+(load "editing.el")
+
+;; Hard-to-categorize customizations
+(load "misc.el")
+
+;; For editing lisps
+(load "elisp-editing.el")
+
+;; Langauage-specific
+(load "setup-clojure.el")
+(load "setup-js.el")
+
+;; THis is where user defined configuration go!
+(global-set-key (kbd "M-p") (kbd "C-u 10 C-p"))
+(global-set-key (kbd "M-n") (kbd "C-u 10 C-n")) 
+
